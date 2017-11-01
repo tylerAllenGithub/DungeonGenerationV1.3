6a76d9c4462d1ef6328a992224de81f3bb7cf461
tylerAllenGithub <tylerallentem@gmail.com>
2017-11-01 11:54:21 -0700

    DungeonGenerationV1.3
    Added Scene Loading

---
+++ Rogue2DTest/Assets/Scripts/Battle/BattleStateMachine.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStateMachine : MonoBehaviour {
+
+
+    public enum PerformAction
+    {
+        WAIT,
+        TAKEACTION,
+        PERFORMACTION
+    }
+
+    public PerformAction battleStates;
+
+    public List<TurnHandler> performList = new List<TurnHandler>();
+    public List<GameObject> playersInBattle = new List<GameObject>();
+    public List<GameObject> enemiesInBattle = new List<GameObject>();
+    
+	// Use this for initialization
+	void Start ()
+    {
+        battleStates = PerformAction.WAIT;
+        enemiesInBattle.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+        playersInBattle.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch (battleStates)
+        {
+            case (PerformAction.WAIT):
+                if(performList.Count > 0)
+                {
+                    battleStates = PerformAction.TAKEACTION;
+                }
+                break;
+
+            case (PerformAction.TAKEACTION):
+                GameObject performer = GameObject.Find(performList[0].attacker);
+                if(performList[0].type == "Enemy")
+                {
+                    EnemyStateMachine ESM = performer.GetComponent<EnemyStateMachine>();
+                    ESM.targetToAttack = performList[0].targetOfAttacker;
+                    ESM.currentState = EnemyStateMachine.TurnState.ACTION;
+                }
+                if (performList[0].type == "Player")
+                {
+
+                }
+                battleStates = PerformAction.PERFORMACTION;
+                break;
+
+            case (PerformAction.PERFORMACTION):
+
+                break;
+        }
+	}
+
+    public void CollectActions(TurnHandler action)
+    {
+        performList.Add(action);
+    }
+}
---
+++ Rogue2DTest/Assets/Scripts/Battle/HeroStateMachine.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroStateMachine : MonoBehaviour {
+
+    public BaseHero hero;
+
+    public enum TurnState
+    {
+        PROCESSING, //waiting for progress bar to fill
+        READY, //add to list, progress bar filled
+        WAITING, //idle
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+
+
+    public TurnState currentState;
+
+    private float currentCoolDown = 0f;
+    private float maxCoolDown = 5f;
+
+    public Image progressBar;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentState = TurnState.PROCESSING;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch(currentState)
+        {
+            case (TurnState.PROCESSING):
+                FillProgressBar();
+                break;
+
+            case (TurnState.READY):
+
+                break;
+
+            case (TurnState.WAITING):
+
+                break;
+
+            case (TurnState.SELECTING):
+
+                break;
+
+            case (TurnState.ACTION):
+
+                break;
+
+            case (TurnState.DEAD):
+
+                break;
+        }
+	}
+
+    void FillProgressBar()
+    {
+        currentCoolDown = currentCoolDown + Time.deltaTime;
+        float calculateCoolDown = currentCoolDown / maxCoolDown;
+        progressBar.transform.localScale = new Vector3(Mathf.Clamp(calculateCoolDown,0,1), progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+        if(currentCoolDown >= maxCoolDown)
+        {
+            currentState = TurnState.READY;
+        }
+    }
+}
